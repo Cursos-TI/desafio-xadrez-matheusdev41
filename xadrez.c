3cb0f909c15cb8c07dc5957eb8b8d64c4d54030b
Matheus Pessoa <matheus41.dev@gmail.com>
2025-02-11 00:26:26 +0000

    Conclusão do nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
+
+// Função Recursiva movimentação do Bispo
+void movimentoBispo(int num) {
+    if (num > 0) {
+        for(int i = 0; i < 1; i++){
+                printf("Direita");
+
+            for(int i = 0; i < 1; i++){
+                printf("/Cima\n");
+
+            }
+        }
+    movimentoBispo(num -1);
+    }
+}
+
+// Função Recursiva movimentação da Torre 
+void movimentoTorre(int num) {
+    if (num > 0) {
+        printf("Direita\n");
+        movimentoTorre(num -1);
+    }
+}
+
+// Função Recursiva movimentação da Rainha 
+void movimentoRainha(int num) {
+    if (num > 0) {
+        printf("Esquerda\n");
+        movimentoRainha(num -1);
+    }
+}
+
+// Função movimentação do cavalo usando variáveis múltiplas
+void movimentoCavalo() {
+    for(int i = 0, j = 3; i < j; i ++, j--){
+        printf("Cima\n");
+    }
+    printf("Direita");
+}
+
 int main()
 {
     // Variaveis para controle de movimentação das peças
@@ -9,36 +49,20 @@ int main()
     int nCasasRainha = 8;
 
     // Simulação da movimentação do Bispo
-    printf("--- Movimento Bispo ---\n"); 
-    for(int i = 0; i < nCasasBispo; i ++){ 
-        printf("Direita/Cima\n");
-    }
+    printf("--- Movimento Bispo ---\n");
+    movimentoBispo(nCasasBispo);
 
     // Simulação da movimentação da Torre
     printf("--- Movimento Torre ---\n"); 
-    for(int i = 0; i < nCasasTorre; i ++){
-        printf("Direita\n");
-    }
+    movimentoTorre(nCasasTorre);
 
     // Simulação da movimentação da Rainha
     printf("--- Movimento Rainha ---\n"); 
-    for(int i = 0; i < nCasasRainha; i ++){
-        printf("Esquerda\n");
-    }
+    movimentoRainha(nCasasRainha);
 
       // Simulação da movimentação do Cavalo
     printf("--- Movimento Cavalo ---\n"); 
-    for(int i = 0; i < 1; i ++){
-
-        while (i < 2)
-        {
-            printf("Baixo\n");
-            i++;
-        }
-
-        printf("esquerda\n");
-    }
+    movimentoCavalo();
 
     return 0;
 }
-
